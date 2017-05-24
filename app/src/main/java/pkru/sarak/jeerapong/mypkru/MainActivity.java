8ff4adb9f2877fcf3e6d7d91090d7680e453159d
kids <monzero753@gmail.com>
2017-05-24 13:06:23 +0700

    Finih 24May 13:05

--- app/src/main/java/pkru/sarak/jeerapong/mypkru/MainActivity.java
+++ app/src/main/java/pkru/sarak/jeerapong/mypkru/MainActivity.java
@@ -1,13 +1,62 @@
 package pkru.sarak.jeerapong.mypkru;
 
+import android.content.Intent;
+import android.graphics.Canvas;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+    //Explicit
+    private EditText userEditText, passwordEditText;
+    private TextView textView;
+    private Button button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Initial Viwe
+        initialViwe();
+
+        // Controller
+        controller();
+
+    } // Main Method
+
+    private void controller() {
+        textView.setOnClickListener(this);
+        button.setOnClickListener(this);
     }
-}
+
+    private void initialViwe() {
+        userEditText = (EditText) findViewById(R.id.edtUser);
+        passwordEditText = (EditText) findViewById(R.id.edtPassword);
+        textView = (TextView) findViewById(R.id.txtNewReginter);
+        button = (Button) findViewById(R.id.btnLogin);
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        //For VTextViwe
+        if (v == textView) {
+            //Intent to NewRegister
+            Intent intent = new Intent(MainActivity.this, NewRegisterActivity.class);
+            startActivity(intent);
+        }
+
+        //For Button
+        if (v == button){
+        }
+    }
+} // Main Class
